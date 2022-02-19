d87c98cc18fc236c9f0d4d694560819156efcb0a
Steph Paterson <steph.c.paterson@gmail.com>
2022-02-19 14:50:23 +0000

    all tests pass

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -6,30 +6,32 @@ from src.card_game import CardGame
 class TestCardGame(unittest.TestCase):
     
     def setUp(self):
-        self.card1 = Card('hearts', 5)
-        self.card2 = Card('spades', 1)
+        self.card1 = Card('hearts', 1)
+        self.card2 = Card('spades', 5)
         self.card_game = CardGame
         self.cards = [self.card1, self.card2]
 
-    @unittest.skip
+
     def test_check_for_ace(self):
         result = self.card_game.check_for_ace(self, self.card1)
         self.assertEqual(True, result)
     
-    @unittest.skip
+
     def test_check_for_ace_false(self):
         result = self.card_game.check_for_ace(self, self.card2)
         self.assertEqual(False, result)
     
-    @unittest.skip
+
     def test_highest_card(self):
         result = self.card_game.highest_card(self, self.card1, self.card2)
         self.assertEqual(self.card2, result)
     
-    @unittest.skip
+
     def test_highest_card_false(self):
-        result = self.card_game.highest_card(self, self.card1, self.card2)
-        self.assertEqual(self.card1, result)
+        self.card3 = Card('hearts', 5)
+        self.card4 = Card('spades', 1)
+        result = self.card_game.highest_card(self, self.card3, self.card4)
+        self.assertEqual(self.card3, result)
 
     def test_cards_total(self):
         result = self.card_game.cards_total(self, self.cards)
